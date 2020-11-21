6e3f4c1af4ef22000700f206c3ee26bdc64060b9
Gowtham G <gowthamgts12@gmail.com>
2020-11-21 23:56:58 +0530

    add toolbar and add immersive full screen

--- app/src/main/java/dev/gowtham/nasapictures/ui/activities/MainActivity.kt
+++ app/src/main/java/dev/gowtham/nasapictures/ui/activities/MainActivity.kt
@@ -1,10 +1,13 @@
 package dev.gowtham.nasapictures.ui.activities
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import dev.gowtham.nasapictures.R
+import dev.gowtham.nasapictures.databinding.ActivityMainBinding
 import timber.log.Timber
 import kotlin.system.exitProcess
 
@@ -22,7 +25,14 @@ class MainActivity : AppCompatActivity() {
             Timber.e(exception, "caught uncaught exception on thread - $paramThread.")
             exitProcess(2)
         }
-        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.attributes.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
+        }
+
+        ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(this.root)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
